fcd08d0ae0fdfd229058ee22c0b8f4b880fe842a
Nikita Kuzminov <nkuzminov220299@gmail.com>
2020-04-26 00:36:40 +0300

    Create flipped cards with effects

---
+++ my-app/js/index.js
@@ -0,0 +1,11 @@
+// memory game flip card
+
+const cards = document.querySelectorAll(".memory-card");
+
+function flipCard() {
+    this.classList.toggle("flip");
+}
+
+cards.forEach(card => card.addEventListener("click", flipCard));
+
+
