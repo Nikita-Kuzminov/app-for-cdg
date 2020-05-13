5174f78dbb365e04a8f6dc346ff3aa28911e9c68
Nikita Kuzminov <nkuzminov220299@gmail.com>
2020-05-13 21:10:31 +0300

    Added function importAllImages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class Card extends React.Component {
     render() {
         return (
             <div className="card">
-                {this.props.img()}
+                {this.props.img(this.props.imgSrc, this.props.Name)}
             </div>
         );
     }
@@ -14,9 +14,16 @@ class Card extends React.Component {
 class Board extends React.Component {
     render() {
         const cards = [];
+        const equalCardsNumber = 2;
+        const imageContext = require.context('../public/img', false, /\.(png|jpe?g|svg)$/);
+        const images = this.importAllImages(imageContext);
 
-        for (let i = 0; i <= 15; i++) {
-            cards.push(<Card img={this.imageCard} />)
+        for (const [imgName, imgSrc] of Object.entries(images)) {
+            for (let i = 0; i <= equalCardsNumber; i++) {
+                cards.push(
+                    <Card img={this.imageCard()} imgName={imgName} imgSrc={imgSrc} />
+                );
+            }
         }
 
         return (
@@ -26,9 +33,17 @@ class Board extends React.Component {
         );
     }
 
-    imageCard() {
-        return <img src="img/angular.svg" alt="Angular"/>
+    imageCard(imgSrc, imgAlt) {
+        return <img src={imgSrc} alt={imgAlt}/>
     }
+
+    importAllImages(context) {
+        let images = {};
+        context.keys().map((item, index) => { images[item.replace('./', '')] = context(item); });
+        return images;
+    }
+
+
 }
 
 
