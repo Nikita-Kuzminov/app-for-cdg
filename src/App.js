36a0063724be469607d818049cbfc7cf729483cb
Nikita Kuzminov <nkuzminov220299@gmail.com>
2020-05-26 11:19:43 +0300

    Add changes in App.js and App.css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,41 @@ import React from 'react';
 import './App.css';
 
 class Card extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            cardIsFlipped: false,
+        };
+    }
+
+    flipCard = () => {
+        if (this.props.numberOfFlippedCards < 2) {
+
+            if (this.props.numberOfFlippedCards === 0) {
+                this.props.setFirstFlippedCard(this.props.imgName);
+            } else {
+                this.props.setSecondFlippedCard(this.props.imgName);
+            }
+
+            const currentState = this.state.cardIsFlipped;
+            this.setState({ cardIsFlipped: !currentState });
+            this.props.setNumberOfFlippedCards();
+        }
+    };
+
     render() {
         return (
-            <div className="card">
-                {this.props.cardImage(this.props.imgSrc, this.props.imgName)}
+            <div className="card" onClick={this.flipCard}>
+                <img src="img/back/js-badge.svg"
+                     alt="js"
+                     className={this.state.cardIsFlipped ? 'img-hidden': null}/>
+
+                <img src={this.props.imgSrc}
+                     alt={this.props.imgName}
+                     className={this.state.cardIsFlipped ? null : 'img-hidden'}/>
+
+                {/*{this.props.cardImage(this.props.imgSrc, this.props.imgName)}*/}
             </div>
         );
     }
@@ -13,6 +44,35 @@ class Card extends React.Component {
 
 
 class Board extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            numberOfFlippedCards: 0,
+            firstFlippedCard: '',
+            secondFlippedCard: '',
+        };
+    }
+
+    checkCardEquals() {
+        const isEqual = this.state.firstFlippedCard === this.state.secondFlippedCard;
+        return isEqual;
+    }
+
+    setNumberOfFlippedCards = () => {
+        const numberOfFlippedCards = this.state.numberOfFlippedCards;
+        this.setState({ numberOfFlippedCards: numberOfFlippedCards + 1 });
+        console.log(this.state.numberOfFlippedCards)
+    }
+
+    setFirstFlippedCard = (alt) => {
+        this.setState({ firstFlippedCard: alt });
+    }
+
+    setSecondFlippedCard = (alt) => {
+        this.setState({ secondFlippedCard: alt });
+    }
+
     render() {
         const cards = [];
         const equalCardsNumber = 1;
@@ -22,7 +82,15 @@ class Board extends React.Component {
         for (const [imgName, imgSrc] of Object.entries(images)) {
             for (let i = 0; i <= equalCardsNumber; i++) {
                 cards.push(
-                    <Card cardImage={this.imageCard} imgSrc={imgSrc} imgName={imgName} />
+                    <Card cardImage={this.imageCard}
+                          imgSrc={imgSrc}
+                          imgName={imgName}
+                          checkcardEquals={this.checkCardEquals}
+                          setNumberOfFlippedCards={this.setNumberOfFlippedCards}
+                          setFirstFlippedCard={this.setFirstFlippedCard}
+                          setSecondFlippedCard={this.setSecondFlippedCard}
+                          numberOfFlippedCards={this.state.numberOfFlippedCards}
+                    />
                 );
             }
         }
@@ -35,7 +103,7 @@ class Board extends React.Component {
     }
 
     imageCard(imgSrc, imgAlt) {
-        return <img src={imgSrc} alt={imgAlt}/>
+        return <img src={imgSrc} alt={imgAlt} className="img-back img-hidden"/>
     }
 
     importAllImages(context) {
@@ -44,16 +112,16 @@ class Board extends React.Component {
         return images;
     }
 
-    
+
 }
 
 
 function App() {
-  return (
-    <div className="App">
-      <Board/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Board/>
+        </div>
+    );
 }
 
 export default App;
